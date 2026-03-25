1a107ed8ce2633e13b34b638ec7cf3d513c8edda
Benjamin Berglund <benjaminberglund@gmail.com>
2026-03-25 08:00:51 +0100

    removed testing from main

--- Testing/SystemTests/playwright-global-setup.js
+++
@@ -1,8 +0,0 @@
-export default async () => {
-  // Global teardown will run after all tests
-  return async () => {
-    if (process.env.SLOW_MODE) {
-      await new Promise(resolve => setTimeout(resolve, 2000));
-    }
-  };
-};
--- Testing/SystemTests/playwright-global-teardown.js
+++
@@ -1,6 +0,0 @@
-export default async () => {
-  if (process.env.SLOW_MODE) {
-    console.log('Waiting 2 seconds before closing...');
-    await new Promise(resolve => setTimeout(resolve, 2000));
-  }
-};
