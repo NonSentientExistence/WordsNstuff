58025aeec1d9f6055259e3c73b6c47dc05b325a7
Max Bergqvist <maxbergqvist@Maxs-MacBook-Pro.local>
2026-04-21 20:43:47 +0200

    use player1Token variable in create-lobby request

--- api-tests/requests/create-lobby.js
+++ api-tests/requests/create-lobby.js
@@ -1,10 +1,12 @@
 export default {
   method: 'POST',
-  url: '{{baseUrl}}/api/lobbies'
+  url: '{{baseUrl}}/api/lobbies',
+  body: {
+    name: 'Testspelaren'
+  }
 };
 
 export function preRequest() {
-  pm.request.headers.add({ key: 'X-Player-Token', value: 'test-host-token' });
   pm.request.headers.add({ key: 'X-Player-Token', value: pm.variables.get('player1Token') });
 }
 
@@ -18,7 +20,5 @@ export function postResponse() {
   pm.test('Lobby code is 6 characters', () =>
     pm.expect(json.code).to.have.lengthOf(6)
   );
-  const json = pm.response.json();
-  pm.test('Response has lobby code', () => pm.expect(json.code).to.be.a('string'));
   pm.variables.set('lobbyCode', json.code);
 }
